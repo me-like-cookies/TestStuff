b6cc7c0217295f164d6796770c0bf78438fb8f19
Me Like Cookies <me.like.cookies@gmx.de>
2020-01-18 10:47:25 +0100

    parametrized JavaFX dependencies added classifier (win32 and linux-arm32) update to 11.0.6 refactoring

--- mvnusejmod/addtolocalmvnrepo.bat
+++ mvnusejmod/addtolocalmvnrepo.bat
@@ -1,10 +1,15 @@
-SET JDKPATH=C:\OpenJDK\bellsoft-jdk-11.0.5-win32_i586
-SET JDKVERSION=11.0.5-Bellsoft+
+REM JDKVERSION=major.minor.micro-supplier
+SET JDKVERSION=11.0.6-Bellsoft+
+REM JDKPLATFORMS: win32, linux-arm32
+SET JDKPLATFORM=win32
+SET JDKPATH=bellsoft-jdk11.0.6-win_i586
+SET JMODPATH=%JDKPATH%\jmods
+SET PARAMS=-Dversion=%JDKVERSION% -Dpackaging=jmod -Dclassifier=%JDKPLATFORM%
 
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.base.jmod -DgroupId=local.openjdk -DartifactId=javafx-base -Dversion=%JDKVERSION% -Dpackaging=jmod
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.controls.jmod -DgroupId=local.openjdk -DartifactId=javafx-controls -Dversion=%JDKVERSION% -Dpackaging=jmod
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.fxml.jmod -DgroupId=local.openjdk -DartifactId=javafx-fxml -Dversion=%JDKVERSION% -Dpackaging=jmod
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.graphics.jmod -DgroupId=local.openjdk -DartifactId=javafx-graphics -Dversion=%JDKVERSION% -Dpackaging=jmod
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.media.jmod -DgroupId=local.openjdk -DartifactId=javafx-media -Dversion=%JDKVERSION% -Dpackaging=jmod
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.swing.jmod -DgroupId=local.openjdk -DartifactId=javafx-swing -Dversion=%JDKVERSION% -Dpackaging=jmod
-mvn install:install-file -Dfile=%JDKPATH%\jmods\javafx.web.jmod -DgroupId=local.openjdk -DartifactId=javafx-web -Dversion=%JDKVERSION% -Dpackaging=jmod
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.base.jmod -DgroupId=local.openjdk -DartifactId=javafx-base %PARAMS%
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.controls.jmod -DgroupId=local.openjdk -DartifactId=javafx-controls %PARAMS%
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.fxml.jmod -DgroupId=local.openjdk -DartifactId=javafx-fxml %PARAMS%
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.graphics.jmod -DgroupId=local.openjdk -DartifactId=javafx-graphics %PARAMS%
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.media.jmod -DgroupId=local.openjdk -DartifactId=javafx-media %PARAMS%
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.swing.jmod -DgroupId=local.openjdk -DartifactId=javafx-swing %PARAMS%
+call mvn install:install-file -Dfile=%JMODPATH%\javafx.web.jmod -DgroupId=local.openjdk -DartifactId=javafx-web %PARAMS%
